853fb3d33c8105100c4764e5ebdde28de2a2ca98
RyanDeLong <rdelong111@gmail.com>
2022-01-24 14:29:06 -0600

    commit tree fc87e8ed5925c1ebd75c7e81208e15ec80ff88da

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  const numI = {};
+  let diff;
+
+  for (let i = 0; i < array.length; i++) {
+    diff = target - array[i];
+
+    if (diff in numI) {
+      return true;
+    }
+    else {
+      numI[array[i]] = i;
+    }
+  }
+
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  O(n)
 */
 
 /* 
-  Add your pseudocode here
+  Initialize an empty object called numI.
+  Initialize diff.
+
+  Iterate over each item in the input array.
+    Set the difference between target and current value of array and put in diff.
+
+    If the value diff is in the object numI;
+      return true.
+    Else;
+      current value in array will be put into the object numI.
+
+  return false.
 */
 
 /*
-  Add written explanation of your solution here
+  Initialize an empty object called numI to store the current value in array if the difference hasn't been iterated through yet.
+  Initialize diff for use when finding the difference between the target and current array value.
+
+  Sets a for loop to iterate through the length of array.
+    Find the difference between target and current value of array and put in diff.
+
+    If the value diff is in the object numI;
+      then it returns true.
+    If the IF statement is false;
+      then the current value in array will be put into the object numI with the current count in the for loop.
+
+    If the for loop runs all the way through, then the function returns false.
 */
 
 // You can run `node index.js` to view these console logs
